a6e442e62c8777acc64723745d4147fb6b76c2e9
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-07-17 21:50:29 -0300

    feat: infra

--- src/Fiap.Workshop.Infrastructure/Repositories/Mappers/ModelMappers.cs
+++ src/Fiap.Workshop.Infrastructure/Repositories/Mappers/ModelMappers.cs
@@ -6,5 +6,5 @@ namespace Fiap.Workshop.Infrastructure.Repositories.Mappers;
 internal static class ModelMappers
 {
     internal static UserModel MapToModel(this User user) =>
-        new(user.Id, user.Email, user.Name, user.Password.Value, user.Role.ToString());
+        new(user.Id, user.Email, user.Name, user.Password.Value, user.Role.ToString(), user.CreatedAt, user.UpdatedAt);
 }
