860b15025963e31b0fa4b42b48ee25262e58c20c
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-06 23:39:51 -0300

    feat: create user api layer

---
+++ src/Fiap.Workshop.Api/Endpoints/Users/UsersEndpoints.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning.Builder;
+using Fiap.Workshop.Api.Mappers;
+using Fiap.Workshop.Api.Requests.Users;
+using Fiap.Workshop.Application.Interfaces.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Workshop.Api.Endpoints.Users;
+
+public static class UsersEndpoints
+{
+    public static void MapUsersEndpoints(this IEndpointRouteBuilder app, ApiVersionSet apiVersion)
+    {
+        var group = app.MapGroup("/users")
+            .WithApiVersionSet(apiVersion)
+            .WithName("Users")
+            .WithTags("Users");
+
+        group.MapPost("",
+            async (
+                [FromBody] CreateUserRequest request,
+                [FromServices] ICreateUserUseCase useCase,
+                CancellationToken cancellationToken
+            ) =>
+            {
+                var result = await useCase.Handle(request.MapToInput(), cancellationToken);
+                if (!result.IsValid)
+                    return Results.BadRequest();
+
+                return Results.Created();
+            }
+        ).RequireAuthorization();
+    }
+}
