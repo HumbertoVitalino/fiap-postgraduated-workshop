3c6d02cf4b4f002a8d38f3871ae0c79ae113418d
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-08 09:07:34 -0300

    feat(mappers): create user

---
+++ src/Fiap.Workshop.Api/Mappers/CreateUserMapper.cs
@@ -0,0 +1,18 @@
+﻿using Fiap.Workshop.Api.Requests.Users;
+using Fiap.Workshop.Application.UseCases.CreateUser.Boundaries;
+
+namespace Fiap.Workshop.Api.Mappers;
+
+public static class CreateUserMapper
+{
+    public static CreateUserInput MapToInput(this CreateUserRequest request)
+    {
+        return new(
+            request.CorrelationId,
+            request.Name,
+            request.Email,
+            request.Password,
+            request.Role
+        );
+    }
+}
