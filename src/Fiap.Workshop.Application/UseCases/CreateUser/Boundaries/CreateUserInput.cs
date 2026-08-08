985626b70b4e209407b27758ed8fb3d33f43774b
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-08 09:07:12 -0300

    feat(boundaries): create user

---
+++ src/Fiap.Workshop.Application/UseCases/CreateUser/Boundaries/CreateUserInput.cs
@@ -0,0 +1,11 @@
+﻿using Fiap.Workshop.Domain.Enums;
+
+namespace Fiap.Workshop.Application.UseCases.CreateUser.Boundaries;
+
+public sealed record CreateUserInput(
+    Guid CorrelationId,
+    string Name,
+    string Email,
+    string Password,
+    UserRole Role
+);
