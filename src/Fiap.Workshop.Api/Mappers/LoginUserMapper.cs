3908038c01142297ffe11eafd1dd64a296e7d82b
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-09 22:18:43 -0300

    feat(api): login user

---
+++ src/Fiap.Workshop.Api/Mappers/LoginUserMapper.cs
@@ -0,0 +1,16 @@
+﻿using Fiap.Workshop.Api.Requests.Auth;
+using Fiap.Workshop.Application.UseCases.LoginUser.Boundaries;
+
+namespace Fiap.Workshop.Api.Mappers;
+
+public static class LoginUserMapper
+{
+    public static LoginUserInput MapToInput(this LoginRequest request)
+    {
+        return new(
+            request.CorrelationId,
+            request.Email,
+            request.Password
+        );
+    }
+}
