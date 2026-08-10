93b59598bab9230d90ee17ac04bf5580f99225d5
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-10 08:19:53 -0300

    feat(mapper): create customer mapper

---
+++ src/Fiap.Workshop.Api/Mappers/CreateCustomerMapper.cs
@@ -0,0 +1,18 @@
+﻿using Fiap.Workshop.Api.Requests.Customers;
+using Fiap.Workshop.Application.UseCases.CreateCustomer.Boundaries;
+
+namespace Fiap.Workshop.Api.Mappers;
+
+public static class CreateCustomerMapper
+{
+    public static CreateCustomerInput MapToInput(this CreateCustomerRequest request)
+    {
+        return new(
+            request.CorrelationId,
+            request.Name,
+            request.Document,
+            request.Email,
+            request.Phone
+        );
+    }
+}
