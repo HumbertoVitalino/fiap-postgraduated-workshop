a6e2c4cc00cdab8b1fe3937fd97d2fc15abb9283
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-10 06:30:12 -0300

    feat(boundaries): create customer usecase

---
+++ src/Fiap.Workshop.Application/Interfaces/UseCases/ICreateCustomerUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Fiap.Workshop.Application.Commons;
+using Fiap.Workshop.Application.UseCases.CreateCustomer.Boundaries;
+
+namespace Fiap.Workshop.Application.Interfaces.UseCases;
+
+public interface ICreateCustomerUseCase
+{
+    Task<Output> Handle(CreateCustomerInput input, CancellationToken cancellationToken);
+}
---
+++ src/Fiap.Workshop.Application/UseCases/CreateCustomer/Boundaries/CreateCustomerInput.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiap.Workshop.Application.UseCases.CreateCustomer.Boundaries;
+
+public sealed record CreateCustomerInput(
+    Guid CorrelationId,
+    string Name,
+    string Document,
+    string Email,
+    string Phone
+);
