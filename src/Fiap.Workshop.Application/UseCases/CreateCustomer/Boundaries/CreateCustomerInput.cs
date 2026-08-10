2d73b69f1724ef93488ea40f55b0e8e6449c8e39
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-10 08:18:55 -0300

    feat(boundaries): create customer

--- src/Fiap.Workshop.Application/UseCases/CreateCustomer/Boundaries/CreateCustomerInput.cs
+++ src/Fiap.Workshop.Application/UseCases/CreateCustomer/Boundaries/CreateCustomerInput.cs
@@ -1,9 +1,31 @@
-﻿namespace Fiap.Workshop.Application.UseCases.CreateCustomer.Boundaries;
+﻿using Fiap.Workshop.Application.Commons;
 
-public sealed record CreateCustomerInput(
-    Guid CorrelationId,
-    string Name,
-    string Document,
-    string Email,
-    string Phone
-);
+namespace Fiap.Workshop.Application.UseCases.CreateCustomer.Boundaries;
+
+public sealed record CreateCustomerInput
+{
+    public Guid CorrelationId { get; init; }
+    public string Name { get; init; }
+    public string Email { get; init; }
+    public string Phone { get; init; }
+    public string Document
+    {
+        get;
+        init => field = value.StandardizeDocument();
+    }
+
+    public CreateCustomerInput(
+        Guid correlationId,
+        string name,
+        string document,
+        string email,
+        string phone
+    )
+    {
+        CorrelationId = correlationId;
+        Name = name;
+        Email = email;
+        Phone = phone;
+        Document = document;
+    }
+}
