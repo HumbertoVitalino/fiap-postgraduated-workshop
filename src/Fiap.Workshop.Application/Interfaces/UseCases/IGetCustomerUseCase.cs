9094d37826215a858473219383aa62d9f0115038
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-11 23:40:48 -0300

    feat(boundaries): get customer

---
+++ src/Fiap.Workshop.Application/Interfaces/UseCases/IGetCustomerUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Fiap.Workshop.Application.Commons;
+using Fiap.Workshop.Application.UseCases.GetCustomer.Boundaries;
+
+namespace Fiap.Workshop.Application.Interfaces.UseCases;
+
+public interface IGetCustomerUseCase
+{
+    Task<Output> Handle(GetCustomerInput input, CancellationToken cancellationToken);
+}
--- src/Fiap.Workshop.Application/IoC/DependencyInjection.cs
+++ src/Fiap.Workshop.Application/IoC/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Fiap.Workshop.Application.Interfaces.UseCases;
 using Fiap.Workshop.Application.UseCases.CreateCustomer;
 using Fiap.Workshop.Application.UseCases.CreateUser;
+using Fiap.Workshop.Application.UseCases.GetCustomer;
 using Fiap.Workshop.Application.UseCases.LoginUser;
 using Microsoft.Extensions.DependencyInjection;
 using System.Diagnostics.CodeAnalysis;
@@ -15,6 +16,7 @@ public static class DependencyInjection
         services.AddScoped<ICreateUserUseCase, CreateUserUseCase>();
         services.AddScoped<ILoginUserUseCase, LoginUserUseCase>();
         services.AddScoped<ICreateCustomerUseCase, CreateCustomerUseCase>();
+        services.AddScoped<IGetCustomerUseCase, GetCustomerUseCase>();
 
         return services;
     }
