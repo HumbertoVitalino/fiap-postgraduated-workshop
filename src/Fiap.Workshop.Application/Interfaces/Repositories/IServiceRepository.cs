2df29c63fddde4ddd5653e0d414aecc4d3b6ff9a
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-15 12:49:02 -0300

    feat(repository): adding new method

--- src/Fiap.Workshop.Application/Interfaces/Repositories/IServiceRepository.cs
+++ src/Fiap.Workshop.Application/Interfaces/Repositories/IServiceRepository.cs
@@ -6,4 +6,5 @@ namespace Fiap.Workshop.Application.Interfaces.Repositories;
 public interface IServiceRepository : IRepository<Service>
 {
     Task<bool> ExistsWithCodeAsync(string code, CancellationToken cancellationToken);
+    Task<IEnumerable<Service>> GetAllAsync(CancellationToken cancellationToken);
 }
