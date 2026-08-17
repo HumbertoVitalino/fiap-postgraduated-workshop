c5eed40cf397c38e6067b270d8c03235cab8d038
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-17 20:45:56 -0300

    feat(uc): new scenarios

--- src/Fiap.Workshop.Application/Interfaces/Repositories/IServiceOrderRepository.cs
+++ src/Fiap.Workshop.Application/Interfaces/Repositories/IServiceOrderRepository.cs
@@ -6,6 +6,7 @@ namespace Fiap.Workshop.Application.Interfaces.Repositories;
 public interface IServiceOrderRepository : IRepository<ServiceOrder>
 {
     Task UpdateAsync(ServiceOrder entity, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<ServiceOrder>> GetAllAsync(CancellationToken cancellationToken);
     Task<IReadOnlyCollection<ServiceOrder>> GetAllByVehicleIdAsync(Guid vehicleId, CancellationToken cancellationToken);
     Task<bool> ExistsWithVehicleIdAsync(Guid vehicleId, CancellationToken cancellationToken);
     Task<bool> ExistsWithServiceIdAsync(Guid serviceId, CancellationToken cancellationToken);
