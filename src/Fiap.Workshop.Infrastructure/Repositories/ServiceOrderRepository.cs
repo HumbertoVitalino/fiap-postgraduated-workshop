e3500ef52d9474d125bb82fc45f5d471217b74a3
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2026-08-17 20:46:03 -0300

    feat(api): endpoints

--- src/Fiap.Workshop.Infrastructure/Repositories/ServiceOrderRepository.cs
+++ src/Fiap.Workshop.Infrastructure/Repositories/ServiceOrderRepository.cs
@@ -86,6 +86,18 @@ internal sealed class ServiceOrderRepository(AppDbContext context) : Repository<
     public override void Remove(ServiceOrder entity) =>
         Delete(_context.ServiceOrders, entity.MapToModel(), entity.Id);
 
+    public async Task<IReadOnlyCollection<ServiceOrder>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var models = await _context.ServiceOrders
+            .AsNoTracking()
+            .Include(so => so.Parts)
+            .Include(so => so.Services)
+            .Include(so => so.StatusHistory)
+            .ToListAsync(cancellationToken);
+
+        return models.Select(model => model.MapToDomain()).ToList();
+    }
+
     public async Task<IReadOnlyCollection<ServiceOrder>> GetAllByVehicleIdAsync(Guid vehicleId, CancellationToken cancellationToken)
     {
         var models = await _context.ServiceOrders
